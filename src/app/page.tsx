08646a8090d3254004017421b1055626d142623f
Nigel Rimando <nfrimando@gmail.com>
2026-04-11 19:40:56 +0800

    fixing robin leading to remco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const WEBSITE_VERSION = "v0.0.21";
+const WEBSITE_VERSION = "v0.0.22";
 
 export default function HomePage() {
   return (
