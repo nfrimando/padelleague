b95a0267fad5526668d1f3aee05d05e3c7782060
Nigel Rimando <nfrimando@gmail.com>
2026-04-11 14:24:02 +0800

    improve player search behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const WEBSITE_VERSION = "v0.0.12";
+const WEBSITE_VERSION = "v0.0.16";
 
 export default function HomePage() {
   return (
